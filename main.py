7d9391212da15757d6999c360fd041d99695a6fb
Brandon Kauffman <brandon.kauffman@pulsarinformatics.com>
2025-01-19 12:04:32 -0500

    init project

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.call_agent import test_method
+
+if __name__ == "__main__":
+    test_method()
---
+++ src/ticket_agent/__init__.py
@@ -0,0 +1,8 @@
+def test_method():
+    """
+    This method is a placeholder for unit tests. It currently prints
+    'test_method'.
+    Adding way more text to scare the linter
+    """
+
+    print("Calling a user")

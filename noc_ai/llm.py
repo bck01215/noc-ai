6bdb53f2df845ed7af0ef81b2dc1f73b9819aa23
Brandon Kauffman <brandon.kauffman@pulsarinformatics.com>
2025-01-23 07:30:39 -0500

    add graph

--- noc_ai/llm.py
+++ noc_ai/llm.py
@@ -1,8 +1,12 @@
 """instantiate llm."""
 
+import json
 import os
+from typing import Any
 
+from elastic_transport import ObjectApiResponse
 from langchain_openai import ChatOpenAI
+from langgraph.graph import END, START, Graph
 from pydantic import SecretStr
 
 from noc_ai.lookups.elastic_stack.agent import ElasticStackAgent
@@ -36,9 +40,19 @@ class NocAiLlm:
             self.es = ElasticStackAgent()
         except ValueError as e:
             raise LLMError("Failed to initialize ElasticStackAgent") from e
-        self.tools = [self.es.search_alert]
+        self.graph_builder = Graph()
+        self.graph_builder.add_node(
+            "get_alert_es_result", self.get_alert_es_result
+        )
+        self.graph_builder.add_node("get_best_result", self.get_best_result)
+        self.graph_builder.add_node("pretty_print_dict", self.pretty_print_dict)
+        self.graph_builder.add_edge(START, "get_alert_es_result")
+        self.graph_builder.add_edge("get_alert_es_result", "get_best_result")
+        self.graph_builder.add_edge("get_best_result", "pretty_print_dict")
+        self.graph_builder.add_edge("pretty_print_dict", END)
+        self.graph = self.graph_builder.compile()
 
-    def determine_alert_action(self, alert_data: str) -> dict:
+    def get_alert_es_result(self, alert_data: str) -> ObjectApiResponse[Any]:
         """
         Determine the action to take based on the alert data.
 
@@ -48,8 +62,15 @@ class NocAiLlm:
         Returns:
             str: The action to take.
         """
-        data = self.es.search_alert(
+        return self.es.search_alert(
             index_pattern="sn-imported", alert_data=alert_data
         )
-        result = self.es.get_best_result(data)
-        return result
+
+    def get_best_result(self, data: ObjectApiResponse[Any]) -> dict:
+        return self.es.get_best_result(data)
+
+    def pretty_print_dict(self, data: dict) -> None:
+        print(json.dumps(data, indent=2))
+
+    def handle_alert(self, alert_data: str) -> None:
+        self.graph.invoke(alert_data)
--- tests/llm/test_llm.py
+++ tests/llm/test_llm.py
@@ -13,4 +13,7 @@ def llm_agent() -> NocAiLlm:
 
 @pytest.mark.skipif(os.getenv("OPENAI_API_KEY") is None, reason="no api key")
 def test_llm_basic_alert(llm_agent: NocAiLlm) -> None:
-    assert 0 == 0
+    res = llm_agent.handle_alert(
+        alert_data="CPU use is 99 percent on getvisprod02.liberty.edu:9100"
+    )
+    assert res
